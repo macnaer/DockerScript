d22ce86e60077b7064c692c853b4cacc25a0cd6a
macnaer <trofimchuk.an@gmail.com>
2021-09-04 11:11:10 +0300

    Update install_jenkins.sh
    Add script

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,1 +1,9 @@
 
+#! /bin/bash
+
+apt -y update 
+apt -y install openjdk-11-jdk
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt -y install jenkins
+apt -y update 
