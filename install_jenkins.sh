703863a9c2917810c9ae324417e13df5e59049ac
macnaer <trofimchuk.an@gmail.com>
2023-05-07 17:27:58 +0300

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -5,5 +5,6 @@ apt -y update
 apt -y install openjdk-11-jdk
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
 apt -y update 
 apt -y install jenkins
